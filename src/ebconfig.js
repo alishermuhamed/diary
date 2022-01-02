02158ed40693447da94c1573eda5da7d8f18a3d8
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2022-01-02 19:07:51 +0600

    Add easybase

---
+++ src/ebconfig.js
@@ -0,0 +1,6 @@
+const ebconfig = {
+  "integration": "project-diary-5LHeCcH1xtcPE8SFWSAy0FSwwvwI-A7m",
+  "version": "2020-11-20"
+}
+
+export default ebconfig;
--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {EasybaseProvider} from 'easybase-react';
+import ebconfig from './ebconfig';
 import {BrowserRouter} from 'react-router-dom';
 import App from './app';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+    <EasybaseProvider ebconfig={ebconfig}>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </EasybaseProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
