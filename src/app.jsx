32b19bf1619b6221f512f3c9a825e052a5c3f888
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2022-01-02 18:58:15 +0600

    Add routing

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,30 @@
 import Header from './components/header';
+import {Routes, Route} from 'react-router-dom';
+import Login from './routes/login';
+import RequireAuth from './components/require-auth';
 
 function App() {
   return <>
     <Header/>
+    <Routes>
+      <Route path="login" element={<Login/>}/>
+      <Route
+        path="add"
+        element={
+          <RequireAuth>
+            <div>Add</div>
+          </RequireAuth>
+        }
+      />
+      <Route
+        path="/"
+        element={
+          <RequireAuth>
+            <div>Home</div>
+          </RequireAuth>
+        }
+      />
+    </Routes>
   </>;
 }
 
