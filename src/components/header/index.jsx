e968713ffe5023240c0b9a10b3538ec48bb5e6ff
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2022-01-03 15:29:19 +0600

    Add link to home on header title

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,8 +1,14 @@
+import {Link} from 'react-router-dom';
 import './style.css';
 
 function Header() {
   return <header className="header">
-    <h1 className="header__title">Дневник</h1>
+    <Link
+      className="header__title"
+      to="/"
+    >
+      <h1>Дневник</h1>
+    </Link>
   </header>;
 }
 
