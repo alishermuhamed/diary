3b58ce4f65be2afbf38a60a6e1a9e19100d0d37a
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2022-01-03 20:54:10 +0600

    Add field wrapper component

---
+++ src/components/field-wrapper/index.jsx
@@ -0,0 +1,15 @@
+import './style.css';
+
+function FieldWrapper(props) {
+  return <div className="field-wrapper">
+    {
+      props.label &&
+      <label htmlFor={props.fieldId}>
+        {props.label}
+      </label>
+    }
+    {props.children}
+  </div>;
+}
+
+export default FieldWrapper;
