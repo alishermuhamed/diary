18f14765ffd637cc84f43e897292925b73987e8f
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2022-01-04 00:17:47 +0600

    Fix showing conditional parts of record

--- src/components/record/index.jsx
+++ src/components/record/index.jsx
@@ -1,17 +1,26 @@
-import './style.css';
 import {MOODS} from '../../constants';
+import './style.css';
 
 function Record(props) {
   const {d, title, mood, text} = props.record;
-
   const date = new Date(d).toLocaleDateString();
 
   return <article className="record">
-    <h2>{title}</h2>
+    <h2>{date}</h2>
 
-    <span>{date}</span>
+    {
+      title &&
+      <h3 className="record__title">
+        {title}
+      </h3>
+    }
 
-    <span>Настроение: {MOODS[mood]}</span>
+    {
+      mood &&
+      <span>
+        Настроение: {MOODS[mood]}
+      </span>
+    }
 
     <p>{text}</p>
   </article>;
