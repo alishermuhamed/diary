acfdcde11cef49757284adcfe246494bbe81d002
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2022-01-03 20:54:22 +0600

    Add login form component

---
+++ src/components/login-form/index.jsx
@@ -0,0 +1,60 @@
+import {useState} from 'react';
+import FieldWrapper from '../field-wrapper';
+import './style.css';
+
+function LoginForm(props) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSubmit = e => {
+    e.preventDefault();
+    props.onSubmit(email, password);
+  };
+
+  return <form
+    className="login-form"
+    onSubmit={onSubmit}
+  >
+    <FieldWrapper
+      fieldId="email"
+      label="Электронная почта"
+    >
+      <input
+        id="email"
+        type="email"
+        required={true}
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+    </FieldWrapper>
+
+    <FieldWrapper
+      fieldId="password"
+      label="Пароль"
+    >
+      <input
+        id="password"
+        type="password"
+        required={true}
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+      />
+    </FieldWrapper>
+
+    {
+      props.error.length > 0 &&
+      <div className="login-form__error">
+        {props.error}
+      </div>
+    }
+
+    <button
+      type="submit"
+      className="login-form__submit-button"
+    >
+      Войти
+    </button>
+  </form>;
+}
+
+export default LoginForm;
