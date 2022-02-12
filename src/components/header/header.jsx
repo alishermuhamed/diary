99419aa6e869c92b281142d375cc7e4ca56a79dd
Alisher Mukhametzhanov <a.mukhametzhanov@icloud.com>
2022-02-12 20:58:51 +0600

    Fix width of log out button on mobile screen

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -5,15 +5,18 @@ import './header.css';
 function Header(props) {
   return <header className="header">
     <Link
-      className="header__title"
+      className="header__title-link"
       to="/"
     >
-      <h1>Дневник</h1>
+      <h1 className="header__title">
+        Дневник
+      </h1>
     </Link>
 
     {
       props.isUserSignedIn &&
       <Button
+        className="header__logout-button"
         onClick={props.onSignOut}
         title="Выйти"
       />
